61deb91c01f21cc5db4f3cc5d0f4d5cececaf391
grrissa <marissanesteban@gmail.com>
2022-02-22 23:33:18 -0800

    asdf

--- game_controls.py
+++ game_controls.py
@@ -26,7 +26,7 @@ def keypress():
             pyautogui.press('right')
 
 
-
+# FIX ME
 def trackpad_mouse():
     ''' 
     Control the game by moving the mouse/finger on trackpad left, right, up, or down. 
@@ -38,7 +38,7 @@ def trackpad_mouse():
         
         global last_position
         global last_dir
-        threshold = 1000
+        threshold = 30
         last_x, last_y = last_position
 
         while not (keyboard.is_pressed("esc")):
@@ -51,11 +51,13 @@ def trackpad_mouse():
                             #pyautogui.press('right')
                             print('right')
                             last_dir = 'right'
+                            last_position = x, y
                     else:
                         if last_dir != 'left':
                             #pyautogui.press('left')
                             print("left")
                             last_dir = 'left'
+                            last_position = x, y
                 
                 if abs(last_y - y) >= threshold:
                     if last_y - y < 0:
@@ -63,11 +65,13 @@ def trackpad_mouse():
                             #pyautogui.press('down')
                             print('down')
                             last_dir = "down"
+                            last_position = x, y
                     else:
                         if last_dir != 'up':
                             #pyautogui.press('up')
                             print('up')
                             last_dir = "up"
+                            last_position = x, y
         
   
 
@@ -83,8 +87,8 @@ def color_tracker():
     import multithreaded_webcam as mw
 
     # You need to define HSV colour range MAKE CHANGE HERE
-    colorLower = (90,46,53)
-    colorUpper = (153,100,100)
+    colorLower = (90,65,53)
+    colorUpper = (138,100,100)
 
     # set the limit for the number of frames to store and the number that have seen direction change
     buffer = 20
@@ -109,12 +113,68 @@ def color_tracker():
         cv2.GaussianBlur(frame, (5,5), 0)
         cv2.cvtColor(frame_flip, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(frame, colorLower, colorUpper)
-        cv2.erode(mask, None, iterations = 2)
-        cv2.dilate(mask, None, iterations = 2)
+        mask1 = cv2.inRange(frame_flip, colorLower, colorUpper)
+        mask2 = cv2.erode(mask1, None, iterations = 2)
+        mask3 = cv2.dilate(mask2, None, iterations = 2)
 
+        list_of_points, item2 = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-        
+        center = None
+
+        largest_contour = max(list_of_points, key = cv2.contourArea)
+        item1, radius = cv2.minEnclosingCircle(largest_contour)
+        M = cv2.moments(largest_contour)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+        if radius > 10:
+            pts.appendleft(radius)
+            
+        if num_frames > 10 and len(pts) > 10:
+            dX, dY = ( pts[0][0]-pts[9][0], (pts[0][1]-pts[9][1] ))
+            threshold = 95
+
+            if dX >= threshold:
+                if  dX < 0:
+                    if last_dir != 'right':
+                        direction = 'right'
+                        last_dir = 'right'
+                else:
+                    if last_dir != 'left':
+                        direction = 'left'
+                        last_dir = 'left'
+            elif dY >= threshold:
+                if  dY < 0:
+                    if last_dir != 'up':
+                        direction = 'up'
+                        last_dir = 'up'
+                else:
+                    if last_dir != 'down':
+                        direction = 'down'
+                        last_dir = 'up'
+
+            cv2.putText(frame_flip, direction, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+
+        if last_dir != 'right' and direction == 'right':
+            #pyautogui.press('right')
+            print("right")
+            last_dir = 'right'
+        if last_dir != 'left' and direction == 'left':
+            #pyautogui.press('left')
+            print("left")
+            last_dir = 'left'
+        if last_dir != 'up' and direction == 'up':
+            #pyautogui.press('up')
+            print('up')
+            last_dir = 'up'
+        if last_dir != 'down' and direction == 'down':
+            #pyautogui.press('down')
+            print('down')
+            last_dir = 'down'
+
+        cv2.imshow('Game Control Window', frame_flip)
+        cv2.waitKey(1)
+        num_frames += 1
+
         continue
         
 
